8bb8a60c65633a431b29359c2cccef48d6eb06bc
sahil <sahilagichani@gmail.com>
2024-02-20 06:04:58 +0530

    Adding all SootUp Dependencies

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,19 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public App(){
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App app = new App();
+        app.appFunc();
+
+    }
+
+    public void appFunc(){
     }
 }
