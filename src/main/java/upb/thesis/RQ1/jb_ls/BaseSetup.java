c47aaa9b3e8da494e8bd063ea6f42fba3499fbc6
sahil <sahilagichani@gmail.com>
2024-06-18 02:46:01 +0530

    dump files in sootup before, after body interceptors

--- src/main/java/upb/thesis/RQ1/jb_ls/BaseSetup.java
+++ src/main/java/upb/thesis/RQ1/jb_ls/BaseSetup.java
@@ -2,17 +2,20 @@ package upb.thesis.RQ1.jb_ls;
 
 import sootup.core.inputlocation.AnalysisInputLocation;
 import sootup.core.model.Body;
+import sootup.core.model.SootClass;
+import sootup.core.model.SootMethod;
 import sootup.core.signatures.MethodSignature;
 import sootup.core.types.ClassType;
 import sootup.java.bytecode.inputlocation.JavaClassPathAnalysisInputLocation;
 import sootup.java.core.JavaSootClass;
 import sootup.java.core.JavaSootMethod;
 import sootup.java.core.interceptors.ConditionalBranchFolder;
-import sootup.java.core.interceptors.CopyPropagator;
 import sootup.java.core.interceptors.LocalSplitter;
 import sootup.java.core.views.JavaView;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -38,6 +41,9 @@ public class BaseSetup {
                 .getMethodSignature("upb.thesis.RQ1.jb_ls.SampleClass", "localSplitterTest", "void", Collections.emptyList());
         System.out.println(methodSignature.getName());
 
+        generateOutputDirs(sootClass, view);
+
+        /*
         Set<? extends JavaSootMethod> sootMethods =  sootClass.getMethods();
         for (JavaSootMethod jsm: sootMethods){
             boolean condition = jsm.getSignature().getSubSignature().getName().equals(methodSignature.getSubSignature().getName());
@@ -52,6 +58,64 @@ public class BaseSetup {
                 //System.out.println(builder.getStmtGraph().toString().trim());
                 System.out.println(builder.getStmtGraph().toString());
             }
+        }
+        */
+    }
+
+    private static void generateOutputDirs(SootClass sootClass, JavaView view) {
+        String userdir = System.getProperty("user.dir");
+        String resDir = userdir + File.separator + "sootupRes";
+        if (!Files.exists(Paths.get(resDir))){
+            File createResDir = new File(resDir);
+            createResDir.mkdir();
+        }
+        String sootClassDir = resDir + File.separator + sootClass.getName();
+        if (!Files.exists(Paths.get(sootClassDir))) {
+            File createSootClassDir = new File(sootClassDir);
+            createSootClassDir.mkdir();
+        }
+
+        Set<? extends SootMethod> sootClassMethods = sootClass.getMethods();
+        for (SootMethod sootClassMethod: sootClassMethods) {
+            String methodName = sootClassMethod.getSignature().getSubSignature().toString();
+            if (methodName.contains("init")){
+                continue;
+            }
+            String sootMethodDir = sootClassDir + File.separator + methodName;
+            if (!Files.exists(Paths.get(sootMethodDir))){
+                File createSootMethodDir = new File(sootMethodDir);
+                createSootMethodDir.mkdir();
+            }
+
+            String input = sootClassMethod.getBody().toString();
+            Body.BodyBuilder builder = Body.builder(sootClassMethod.getBody(), Collections.emptySet());
+            new LocalSplitter().interceptBody(builder, view);
+
+            String output = builder.getStmtGraph().toString();
+
+            generateOutput(sootMethodDir, input, output);
+        }
+    }
+
+    private static void generateOutput(String sootMethodDir, String input, String output) {
+
+        // Create a File object for the file
+        File file = new File(sootMethodDir + File.separator + "input");
+        // Write content to the file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(input);
+            //System.out.println("File created and content written successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the input file.");
+            e.printStackTrace();
+        }
+
+        File outfile = new File(sootMethodDir + File.separator + "output");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outfile))) {
+            writer.write(output);
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the output file.");
+            e.printStackTrace();
         }
     }
 
--- src/main/java/upb/thesis/RQ1/jb_ls/SampleClass.java
+++ src/main/java/upb/thesis/RQ1/jb_ls/SampleClass.java
@@ -1,11 +1,86 @@
 package upb.thesis.RQ1.jb_ls;
 
 public class SampleClass {
-    void localSplitterTest() {
+    int global = 10;
+
+    //Test Case 1: Simple Assignment
+    void tc1(){
+        int a, b;
+        a = 1;
+        b = a;
+        b = b + 1;
+    }
+
+    //Test Case 2: Compound Assignment
+    void tc2(){
+        int a, b;
+        a = 1;
+        b = a + 3;
+    }
+
+    //Test Case 3: Conditional Statements
+    void tc3(){
+        int a, b;
+        a = 1;
+        if (a == 2){
+            b = 5;
+        } else {
+            b = 10;
+        }
+        int x = b + 1;
+    }
+
+    //Test Case 4: Loops
+    void tc4(){
+        int a = 0;
+        int b;
+        for (int i = 0; i < 3; i++){
+            b = a + i;
+        }
+    }
+
+    //Test Case 5: Switch Case
+    void tc5(){
+        int a = 3;
+        switch (a){
+            case 1: a = a + 1; int b = 1; break;
+            case 2: a = a + 1; b = 2; break;
+            case 3: a = a + 1; b = 3; break;
+            default: a = a - 1;
+        }
+    }
+
+    //Test Case 6: Variable Shadowing
+    void tc6(){
+        {
+            int b = 2;
+        }
+        int b = 5;
+    }
+
+    //Test Case 7: Error Handling
+    void tc7(){
+        int a = 8/0;
+        int b = a + 2;
+    }
+
+    //Test Case 8: Global var
+    void tc8(){
+        int a = global + 5;
+    }
+
+    //Test Case 9: String Concat
+//    void tc9(){
+//        String x = "abc";
+//        String y = "bcd";
+//        String z = x + y;
+//    }
+
+    void localSplitterTest(){
         int x = 0;
-        if (x > 0) {
+        if (x > 0){
             x++;
-        } else if (x + 10 > 21) {
+        } else if (x + 10 > 21){
             x--;
         } else {
             System.out.println("Do Nothing");
